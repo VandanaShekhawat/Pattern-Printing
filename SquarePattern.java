f534be453a6242bad36d7089b47c6a988d47792d
VandanaShekhawat <115356315+VandanaShekhawat@users.noreply.github.com>
2022-10-12 20:30:44 +0530

    Update SquarePattern.java

--- SquarePattern.java
+++ SquarePattern.java
@@ -1,1 +1,18 @@
 
+public class MyClass {
+    public static void main(String args[]) {
+     for (int i = 0; i<5;i++)
+        {
+            for(int j =0;j<5;j++){
+                if( i==0 || i==5-1 || j==0 || j==5-1){
+                     System.out.print("*"); 
+                }
+                else {
+                    System.out.print(" ");
+                }
+              
+            }
+            System.out.println();
+        }
+    }
+}
